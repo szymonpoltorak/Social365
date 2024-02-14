38301cae09b56aeb85ea36bfbd52d27cc77afd6a
Szymon Półtorak <szymonpotorak@gmail.com>
2024-02-14 17:33:28 +0100

    Added password policy

--- services/authorisation/keycloak-initializer/src/main/java/keycloakinitializer/realm/corn/Social365Realm.java
+++ services/authorisation/keycloak-initializer/src/main/java/keycloakinitializer/realm/corn/Social365Realm.java
@@ -14,9 +14,22 @@ public class Social365Realm extends RealmRepresentation {
         setRegistrationAllowed(true);
         setClients(List.of(new Social365Client()));
         setIdentityProviders(ExternalConfig.getIdentityProviders());
+        setPasswordPolicy(buildPasswordPolicy());
 
         if(ExternalConfig.KCCFG_LOGIN_THEME_NAME != null) {
             setLoginTheme(ExternalConfig.KCCFG_LOGIN_THEME_NAME);
         }
+    }
+
+    private String buildPasswordPolicy() {
+        String length = "8";
+        String digits = "1";
+        String specialChars = "1";
+        String upperCase = "1";
+        String lowerCase = "1";
+        String notUsername = "1";
+
+        return String.format("%s and %s and %s and %s and %s and %s",
+                length, digits, specialChars, upperCase, lowerCase, notUsername);
     }
 }
