2522e220a1a0580e571c2e1c99262ef8c18cd222
Szymon Półtorak <szymonpotorak@gmail.com>
2024-02-15 21:43:16 +0100

    Configured eureka and config servers

--- services/eureka-registry/src/main/java/razepl/dev/social365/registry/EurekaRegistryApplication.java
+++ services/eureka-registry/src/main/java/razepl/dev/social365/registry/EurekaRegistryApplication.java
@@ -2,7 +2,9 @@ package razepl.dev.social365.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaRegistryApplication {
 
