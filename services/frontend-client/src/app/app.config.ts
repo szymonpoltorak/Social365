3e17bd0b20cfb7904fa8114c658cc5b95b2a4909
Szymon Półtorak <szymonpotorak@gmail.com>
2024-02-18 09:59:12 +0100

    Reinitialized frontend app without ssr

--- services/frontend-client/src/app/app.config.ts
+++ services/frontend-client/src/app/app.config.ts
@@ -1,13 +1,12 @@
-import {ApplicationConfig, isDevMode} from '@angular/core';
-import {provideRouter} from '@angular/router';
+import { ApplicationConfig, isDevMode } from '@angular/core';
+import { provideRouter } from '@angular/router';
 
-import {routes} from './app.routes';
-import {provideClientHydration} from '@angular/platform-browser';
-import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
-import {provideServiceWorker} from '@angular/service-worker';
+import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), provideServiceWorker('ngsw-worker.js', {
+    providers: [provideRouter(routes), provideAnimationsAsync(), provideServiceWorker('ngsw-worker.js', {
         enabled: !isDevMode(),
         registrationStrategy: 'registerWhenStable:30000'
     })]
