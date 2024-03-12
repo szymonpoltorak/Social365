7d064fadfe03621736a826a6e2d2e07a92557265
Szymon Półtorak <szymonpotorak@gmail.com>
2024-03-12 20:55:49 +0100

    Styled friends profile tab

---
+++ services/frontend-client/src/app/core/data/profile/FriendProfileOption.ts
@@ -0,0 +1,6 @@
+export interface FriendProfileOption {
+    username: string;
+    fullName: string;
+    mutualFriends: number;
+    imageLink: string;
+}
---
+++ services/frontend-client/src/app/pages/profile/profile-friends/friend-profile/friend-profile.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input } from '@angular/core';
+import { FriendProfileOption } from "@core/data/profile/FriendProfileOption";
+import { MatIconButton } from "@angular/material/button";
+import { MatIcon } from "@angular/material/icon";
+import { MatTooltip } from "@angular/material/tooltip";
+import { MatMenu, MatMenuItem, MatMenuTrigger } from "@angular/material/menu";
+
+@Component({
+    selector: 'app-friend-profile',
+    standalone: true,
+    imports: [
+        MatIconButton,
+        MatIcon,
+        MatTooltip,
+        MatMenuTrigger,
+        MatMenu,
+        MatMenuItem
+    ],
+    templateUrl: './friend-profile.component.html',
+    styleUrl: './friend-profile.component.scss'
+})
+export class FriendProfileComponent {
+    @Input() friend!: FriendProfileOption;
+
+    getMutualFriends() {
+        return `Szymon,&#13
+        Andrzej,&#13
+        Kasia,&#13
+        Maciek&#13
+        Asia`;
+    }
+}
