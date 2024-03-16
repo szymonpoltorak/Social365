44141d7e574adda1f6f81eb217889a7c8fc89134
Szymon Półtorak <szymonpotorak@gmail.com>
2024-03-16 13:21:17 +0100

    Styled photos page

--- services/frontend-client/src/app/pages/profile/profile-photos/profile-photos.component.ts
+++ services/frontend-client/src/app/pages/profile/profile-photos/profile-photos.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { MatCard, MatCardContent, MatCardHeader, MatCardTitle } from "@angular/material/card";
+import { MatIconButton } from "@angular/material/button";
+import { MatIcon } from "@angular/material/icon";
+import { MatMenu, MatMenuItem, MatMenuTrigger } from "@angular/material/menu";
 
 @Component({
-  selector: 'app-profile-photos',
-  standalone: true,
-  imports: [],
-  templateUrl: './profile-photos.component.html',
-  styleUrl: './profile-photos.component.scss'
+    selector: 'app-profile-photos',
+    standalone: true,
+    imports: [
+        MatCard,
+        MatCardHeader,
+        MatCardTitle,
+        MatCardContent,
+        MatIconButton,
+        MatIcon,
+        MatMenu,
+        MatMenuTrigger,
+        MatMenuItem
+    ],
+    templateUrl: './profile-photos.component.html',
+    styleUrl: './profile-photos.component.scss'
 })
 export class ProfilePhotosComponent {
-
+    protected photos: string[] = ["1", "2", "3", "4", "5", "6", "7"];
 }
