7c57d883cba9f9f7d111eeb910ebb6c219232237
Szymon Półtorak <szymonpotorak@gmail.com>
2024-04-16 18:46:57 +0200

    Transfer to pc

--- services/frontend-client/src/app/pages/friends/friend-requests/friend-requests.component.ts
+++ services/frontend-client/src/app/pages/friends/friend-requests/friend-requests.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from "@angular/material/card";
+import { MatButton } from "@angular/material/button";
 
 @Component({
-  selector: 'app-friend-requests',
-  standalone: true,
-  imports: [],
-  templateUrl: './friend-requests.component.html',
-  styleUrl: './friend-requests.component.scss'
+    selector: 'app-friend-requests',
+    standalone: true,
+    imports: [
+        MatCardModule,
+        MatButton
+    ],
+    templateUrl: './friend-requests.component.html',
+    styleUrl: './friend-requests.component.scss'
 })
 export class FriendRequestsComponent {
 
