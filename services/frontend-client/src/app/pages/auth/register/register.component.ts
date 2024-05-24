f12b7b6e08e71c931056ad6be17ee885b4f50324
Szymon Półtorak <szymonpotorak@gmail.com>
2024-05-24 19:57:32 +0200

    Styled auth pages

--- services/frontend-client/src/app/pages/auth/register/register.component.ts
+++ services/frontend-client/src/app/pages/auth/register/register.component.ts
@@ -4,7 +4,15 @@ import { MatError, MatFormField } from "@angular/material/form-field";
 import { MatIcon } from "@angular/material/icon";
 import { MatInputModule } from "@angular/material/input";
 import { NgIf } from "@angular/common";
-import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
+import {
+    AbstractControl,
+    FormBuilder,
+    FormControl,
+    FormGroup,
+    ReactiveFormsModule,
+    ValidatorFn,
+    Validators
+} from "@angular/forms";
 import { RouterLink } from "@angular/router";
 import { MatDatepickerModule } from "@angular/material/datepicker";
 
@@ -27,23 +35,59 @@ import { MatDatepickerModule } from "@angular/material/datepicker";
     styleUrl: './register.component.scss'
 })
 export class RegisterComponent implements OnInit {
+    private readonly PASSWORD_PATTERN: string = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[:\\?\\.@!#:\\-_=+ ])[a-zA-Z0-9:\\?\\.@!#:\\-_=+ ]{8,32}$";
     registerForm !: FormGroup;
+    passwordsGroup !: FormGroup;
 
     constructor(private formBuilder: FormBuilder) {
     }
 
+    passwordMatchValidator(passwordFieldName: string, repeatFieldName: string): ValidatorFn {
+        return (control: AbstractControl): { [key: string]: any } | null => {
+            const userPassword: AbstractControl<any, any> | null = control.get(passwordFieldName);
+            const repeatPassword: AbstractControl<any, any> | null = control.get(repeatFieldName);
+
+            return userPassword && repeatPassword && userPassword.value !== repeatPassword.value ? { 'passwordMatch': true } : null;
+        };
+    }
+
+    properAgeValidator(): ValidatorFn {
+        return (control: AbstractControl): { [key: string]: any } | null => {
+            const currentDateYear: number = new Date().getFullYear();
+            const dateFieldValueYear: number = new Date(control.value).getFullYear();
+            const userAge: number = currentDateYear - dateFieldValueYear;
+            const maxAge: number = 200;
+            const minAge: number = 13;
+            const condition: boolean = userAge > maxAge || userAge < minAge || dateFieldValueYear > currentDateYear;
+
+            return condition ? { 'wrongAge': true } : null;
+        };
+    }
+
     ngOnInit(): void {
+        this.passwordsGroup = this.formBuilder.group({
+            password: new FormControl<string>("", [
+                Validators.required, Validators.minLength(6), Validators.maxLength(100),
+                Validators.pattern(this.PASSWORD_PATTERN)
+            ]),
+            confirmPassword: new FormControl<string>("", [
+                Validators.required, Validators.minLength(6), Validators.maxLength(100),
+                Validators.pattern(this.PASSWORD_PATTERN)
+            ]),
+        }, {
+            validators: [
+                this.passwordMatchValidator("password", "confirmPassword"),
+            ]
+        });
+
         this.registerForm = this.formBuilder.group({
             email: new FormControl<string>("", [
                 Validators.required, Validators.email, Validators.minLength(3), Validators.maxLength(100)
             ]),
-            passwords: {
-                password: new FormControl<string>("", [
-                    Validators.required, Validators.minLength(6), Validators.maxLength(100),
-                    Validators.pattern("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[:\\?\\.@!#:\\-_=+ ])[a-zA-Z0-9:\\?\\.@!#:\\-_=+ ]{8,32}$")
-                ]),
-                confirmPassword: new FormControl<string>("", [])
-            },
+            passwords: this.passwordsGroup,
+            dateOfBirth: new FormControl<string>("", [
+                Validators.required, this.properAgeValidator()
+            ]),
         });
     }
 
