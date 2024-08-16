e91d882264dc3ddaea411d0eb25eea27d57dac21
Szymon Półtorak <szymonpotorak@gmail.com>
2024-08-16 14:07:30 +0200

    Lowered the ram usage of cassandra heap allocation, moved images-service and auth-service prod builds to graalvm

--- scripts/rpd.sh
+++ scripts/rpd.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+docker build -t social-native:base .
+
 docker compose -f ../compose.prod.yml --env-file=../services/.env up -d --build
--- scripts/sdv.sh
+++ scripts/sdv.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 docker compose -f ../compose.dev.yml --env-file=../services/.env down --rmi all --remove-orphans
+
+docker image rm social-native:base
