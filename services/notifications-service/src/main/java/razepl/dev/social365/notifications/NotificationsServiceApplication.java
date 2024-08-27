4bc2d01ecaca7efbb2f60570c969f36404f12776
Szymon Półtorak <szymonpotorak@gmail.com>
2024-08-27 20:28:38 +0200

    Initialized notifications project

---
+++ services/notifications-service/src/main/java/razepl/dev/social365/notifications/NotificationsServiceApplication.java
@@ -0,0 +1,13 @@
+package razepl.dev.social365.notifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationsServiceApplication.class, args);
+    }
+
+}
